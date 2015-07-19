740d944da6c17bb839974b3fb20215ee9371564d
pavelshmidt <pavelshmidt@github.com>
2015-07-19 16:27:51 +0300

    save2

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,24 @@
 'use strict';
 
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-//
-// gulp.task('sass', function () {
-//   gulp.src('src/sass/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('src/css'));
-// });
-//
-// gulp.task('watch', function () {
-//   gulp.watch('src/sass/**/*.scss', ['sass']);
-// });
-
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
-// Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./app"
+        server: "./src"
     });
 
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("src/sass/*.scss", ['sass']);
+    gulp.watch("src/**/*.*").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
+    return gulp.src("src/sass/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
